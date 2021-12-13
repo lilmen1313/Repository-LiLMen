6aa52202b8c37ee6f9c2cc904c55f4fe278660e6
lilmen1313 <lilmen1313@gmail.com>
2021-12-13 12:55:06 +0300

    Выполнил задание 2

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -2,5 +2,11 @@
 // 2. Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) 
 //по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.Write("Введите свой вес в кг: ");
+string m = Console.ReadLine();
+double m1 = Convert.ToDouble(m);
+Console.Write("Введите свой рост в м: ");
+string h = Console.ReadLine();
+double h1 = Convert.ToDouble(h);
+double imt = m1/(h1*h1);
+Console.WriteLine("Индекс массы тела: {0:F3}", imt);
