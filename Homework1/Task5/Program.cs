56be4f51aef7c8095601b3a2e3966a7d1633e72a
lilmen1313 <lilmen1313@gmail.com>
2021-12-14 16:53:48 +0300

    Попытка скрыть мусор

--- Homework1/Task5/Program.cs
+++ Homework1/Task5/Program.cs
@@ -1,11 +1,18 @@
-﻿
+﻿// ФИ: Асанов Илья
+// 5.
+// а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+// б) Сделать задание, только вывод организовать в центре экрана.
+// в) *Сделать задание б с использованием собственных методов (например, Print(string ms, int x,int y).
 
-        static void Print(string msg, int x, int y)
-        {
-            // Установим позицию курсора на экране
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine(msg);
-        }
+//         static void Print(string msg, int x, int y)
+//         {
+//             // Установим позицию курсора на экране
+//             Console.SetCursorPosition(x, y);
+//             Console.WriteLine(msg);
+//         }
+// Print ("ВАЙ ВАЙ КАРАВАЙ", 87, 0);
+
+
 
         // static void Print(string msg, ConsoleColor foregroundcolor)
         // {
@@ -15,13 +22,11 @@
         // }
 
 
-        //    // Создайте перегрузку функции, в которой будет указана позиция и цвет
-        // static void Main()
-        // {
-        //    Print("Привет!\n", 10,10);
-        //    Print("Привет еще раз!", ConsoleColor.Red);
-        //   Console.ReadLine();
-        // }
-
-
-Print (ghj, 6, 5);
+           // Создайте перегрузку функции, в которой будет указана позиция и цвет
+        static void Main()
+        {
+           Print("Привет!\n", 10,10);
+           Print("Привет еще раз!", ConsoleColor.Red);
+          Console.ReadLine();
+        }
+Main();
