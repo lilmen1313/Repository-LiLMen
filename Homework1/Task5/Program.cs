541e3520d3e1e781cb189394fd0b278f24359217
lilmen1313 <lilmen1313@gmail.com>
2021-12-14 13:14:11 +0300

    Добавлен ГитИгнор и папка для Задания 5

---
+++ Homework1/Task5/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+        static void Print(string msg, int x, int y)
+        {
+            // Установим позицию курсора на экране
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(msg);
+        }
+
+        // static void Print(string msg, ConsoleColor foregroundcolor)
+        // {
+        //     // Установим цвет символов
+        //     Console.ForegroundColor = foregroundcolor;
+        //     Console.WriteLine(msg);
+        // }
+
+
+        //    // Создайте перегрузку функции, в которой будет указана позиция и цвет
+        // static void Main()
+        // {
+        //    Print("Привет!\n", 10,10);
+        //    Print("Привет еще раз!", ConsoleColor.Red);
+        //   Console.ReadLine();
+        // }
+
+
+Print (ghj, 6, 5);
