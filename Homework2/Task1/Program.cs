55a78d2ce1fd7c6201b1205db01b33fc1f3470a3
lilmen1313 <lilmen1313@gmail.com>
2021-12-15 13:56:47 +0300

    Добавлена папка ДЗ 2 и задание 1 из него

---
+++ Homework2/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// ФИ: Асанов Илья
+// 1. Написать метод, возвращающий минимальное из трёх чисел.
+
+void Min3 ()
+{
+double a, b, c, min, min1;
+Console.Write("Введите число а = ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число b = ");
+b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число c = ");
+c = Convert.ToDouble(Console.ReadLine());
+if (a<b)
+{
+    min1 = a;
+}
+else {
+    min1 = b;
+};
+if (c<min1)
+{
+    min = c;
+}
+else {
+    min = min1;
+};
+Console.Write("Наименьшее число = {0}",min);
+};
+
+Min3 (); // вызов метода, возвращающего наименьшее число из трёх введенных
