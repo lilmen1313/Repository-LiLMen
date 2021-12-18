bcf5f881030bc2166ef640c69b7ec682d87610ca
lilmen1313 <lilmen1313@gmail.com>
2021-12-19 00:47:17 +0300

    Добавил задачу 2 ДЗ№2

--- Homework2/Task2/Program.cs
+++ Homework2/Task2/Program.cs
@@ -1,4 +1,19 @@
 ﻿// ФИ: Асанов Илья
-//  2. Написать метод подсчета количества цифр числа.
+// 2. Написать метод подсчета количества цифр числа.
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите целое число и узнаете количество цифр в нем: a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+int ch = 1;
+int chh = 1;
+while (a != 0)
+{
+chh = ch++;
+a = a / 10;
+}
+Console.Write($"Количество цифр в числе a = {chh}");
+
+// Console.Write("Введите число и узнаете количество цифр в нем: a = ");
+// string a = Console.ReadLine();
+//  char[] c = a.ToCharArray();
+// // bool ver;
+// Console.Write($"Количество цифр в числе a = {c}");
